fc915383b34830e13c6b4b3a99f63cbfaa29f75f
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-06-08 14:53:59 -0300

    pomodoro

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ let running = true;
 let data = { todos : [] };
 
 function app() {
-    const options = ['add', 'list', 'check', 'remove'];
+    const options = ['add', 'list', 'check', 'pomodoro', 'remove'];
     const index = readlineSync.keyInSelect(options, 'Type your command');
 
     switch (index) {
@@ -21,8 +21,12 @@ function app() {
         case 2:
             check();
             break;
-        
+
         case 3:
+            pomodoro();
+            break;
+        
+        case 4:
             remove();
             break;
 
@@ -57,6 +61,21 @@ function check() {
         data.todos[index].check = !data.todos[index].check;
     } else {
         console.log("No todo to uncheck");
+        readlineSync.promptCL();
+    }
+}
+
+function pomodoro() {
+    if(data.todos.length > 0) {
+        const index = readlineSync.keyInSelect(data.todos.map(todo => todo.name), 'What todo do you want to have a pomodoro? ');
+        if (data.todos[index].name.includes(" 🍅")){
+            console.log("No todo to pomodoro");
+            readlineSync.promptCL();
+        } else {
+            data.todos[index].name += " 🍅";
+        }
+    } else {
+        console.log("No todo to pomodoro");
         readlineSync.promptCL();
     }
 }
